521e95133ac27508936976d1b4058b265fa9e24e
shoom3301 <shoom3301@gmail.com>
2022-04-05 18:48:22 +0700

    Render data by angular way

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,46 +15,18 @@ export class AppComponent implements AfterViewInit {
   title = '1inch Limit Order Parser';
   @ViewChild('editor') editor:any;
 
+  parsedData: any = null;
+
   constructor() { }
 
   ngAfterViewInit() {
     let aceEditor = ace.edit(this.editor.nativeElement);
-
-
   }
 
   async onClick() {
-    let aceEditor = ace.edit(this.editor.nativeElement);
-    let text = aceEditor.getValue();
-    let data = Parser(text);
-    let output = await buildOutput(data);
-    // write output to the box-output
-    const outputBox = document.getElementById('json-output');
-      if(outputBox!=null)
-      {
-          outputBox.innerHTML = output;
-      }
+    const aceEditor = ace.edit(this.editor.nativeElement);
+    const text = aceEditor.getValue();
 
-
+    this.parsedData = Parser(text);
   }
 }
-
-
-// @Component({
-//   selector: 'app-root',
-//   templateUrl: './app.component.html',
-//   styleUrls: ['./app.component.css']
-// })
-
-
-// export class AppComponent implements AfterViewInit {
-//   title = '1inch Limit Order Parser';
-
-//   @ViewChild("editor") private editor:any;
-
-//   ngAfterViewInit() {
-//     var editor = ace.edit(this.editor.nativeElement);
-//     Parser(editor);
-//   }
-// }
-
